f15522c680f7c908b7fd3595113157471c79894f
Oskar Ingberg <oscar.ingberg@mrgreen.com>
2017-10-27 14:38:32 +0200

    Uppdate server to allow Access-Control-Allow-Origin

--- server.js
+++ server.js
@@ -12,17 +12,28 @@ mongoose.connect('mongodb://Filip:Ninetech17!@ds231315.mlab.com:31315/todo-list'
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'dev')));
 
-app.use("/js", express.static(__dirname + 'dev/js'));
-console.log(__dirname);
+app.use(function (req, res, next) {
+  
+      // Website you wish to allow to connect
+      res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+  
+      // Request methods you wish to allow
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  
+      // Request headers you wish to allow
+      res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  
+      // Set to true if you need the website to include cookies in the requests sent
+      // to the API (e.g. in case you use sessions)
+      res.setHeader('Access-Control-Allow-Credentials', true);
+  
+      // Pass to next layer of middleware
+      next();
+  });
 
 var routes = require('./api/routes/todoListRoutes');
 routes(app);
-
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dev/index.html'));
-});
 
 app.use(function(req, res) {
   res.status(404).send({url: req.originalUrl + ' not found'})
